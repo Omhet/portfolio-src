9644d843249c8a72e226b5405d5107ae6bce2e7a
Omhet <omhetehmo@yandex.ru>
2019-09-19 22:29:35 +0400

    Remove trash

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,11 @@
 import React from "react"
 
-import { rhythm, scale } from "../utils/typography"
-
 class Layout extends React.Component {
   render() {
     const { children } = this.props
 
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <div>
         <main>{children}</main>
       </div>
     )
